3ac2d0909d024141abdd50f386ba1e8e2cf3707f
Paul Kienzle <pkienzle@nist.gov>
2017-09-06 12:07:47 -0400

    convert LSAT example to python

--- examples/_py/lsat.py
+++ examples/_py/lsat.py
@@ -3,36 +3,36 @@ LSAT: item response
 
 ::
 
-	model
-	{
-	# Calculate individual (binary) responses to each test from multinomial data
-		for (j in 1 : culm[1]) {
-			for (k in 1 : T) {
-				r[j, k] <- response[1, k]
-			}
-		}
-		for (i in 2 : R) {
-			for (j in culm[i - 1] + 1 : culm[i]) {
-				for (k in 1 : T) {
-					r[j, k] <- response[i, k]
-				}
-			}
-		}
-	# Rasch model
-		for (j in 1 : N) {
-			for (k in 1 : T) {
-				logit(p[j, k]) <- beta * theta[j] - alpha[k]
-				r[j, k] ~ dbern(p[j, k])
-			}
-			theta[j] ~ dnorm(0, 1)
-		}
-	# Priors
-		for (k in 1 : T) {
-			alpha[k] ~ dnorm(0, 0.0001)
-			a[k] <- alpha[k] - mean(alpha[])
-		}
-		beta ~ dflat()T(0, )
-	}
+    model
+    {
+    # Calculate individual (binary) responses to each test from multinomial data
+        for (j in 1 : culm[1]) {
+            for (k in 1 : T) {
+                r[j, k] <- response[1, k]
+            }
+        }
+        for (i in 2 : R) {
+            for (j in culm[i - 1] + 1 : culm[i]) {
+                for (k in 1 : T) {
+                    r[j, k] <- response[i, k]
+                }
+            }
+        }
+    # Rasch model
+        for (j in 1 : N) {
+            for (k in 1 : T) {
+                logit(p[j, k]) <- beta * theta[j] - alpha[k]
+                r[j, k] ~ dbern(p[j, k])
+            }
+            theta[j] ~ dnorm(0, 1)
+        }
+    # Priors
+        for (k in 1 : T) {
+            alpha[k] ~ dnorm(0, 0.0001)
+            a[k] <- alpha[k] - mean(alpha[])
+        }
+        beta ~ dflat()T(0, )
+    }
 """
 
 from bumps.names import *
@@ -89,11 +89,11 @@ problem.visible_vars = post_vars + ["beta"]
 
 
 openbugs_result = """
-		mean	sd	MC_error	val2.5pc	median	val97.5pc	start	sample
-	a[1]	-1.26	0.1053	0.001266	-1.474	-1.26	-1.056	1001	10000
-	a[2]	0.4776	0.0698	8.158E-4	0.3412	0.4776	0.6168	1001	10000
-	a[3]	1.239	0.0687	9.116E-4	1.106	1.239	1.374	1001	10000
-	a[4]	0.1696	0.07325	8.07E-4	0.02688	0.1692	0.313	1001	10000
-	a[5]	-0.6256	0.08617	0.001083	-0.7961	-0.6239	-0.4563	1001	10000
-	beta	0.7582	0.07181	0.001678	0.6125	0.7601	0.895	1001	10000
+         mean     sd       MC_error   2.5pc    median    97.5pc   start  sample
+  a[1]  -1.26     0.1053   0.001266  -1.474    -1.26     -1.056    1001   10000
+  a[2]   0.4776   0.0698   8.158E-4   0.3412    0.4776    0.6168   1001   10000
+  a[3]   1.239    0.0687   9.116E-4   1.106     1.239     1.374    1001   10000
+  a[4]   0.1696   0.07325  8.07E-4    0.02688   0.1692    0.313    1001   10000
+  a[5]  -0.6256   0.08617  0.001083  -0.7961   -0.6239   -0.4563   1001   10000
+  beta   0.7582   0.07181  0.001678   0.6125    0.7601    0.895    1001   10000
 """
--- examples/_py/lsat_fast.py
+++ examples/_py/lsat_fast.py
@@ -11,47 +11,47 @@ The OpenBUGS model would be adjusted as follows:
 
 ::
 
-	model
-	{
-	# Calculate individual (binary) responses to each test from multinomial data
-		for (j in 1 : culm[1]) {
-			for (k in 1 : T) {
-				r[j, k] <- response[1, k]
-			}
-		}
-		for (i in 2 : R) {
-			for (j in culm[i - 1] + 1 : culm[i]) {
-				for (k in 1 : T) {
-					r[j, k] <- response[i, k]
-				}
-			}
-		}
-	# Treat students with the same reponse profile as having the same latent ability
-		for (i in 1 : R) {
-			ability[i] ~ dnorm(0, 1)
-		}
-		for (j in 1 : culm[1]) {
-			theta[j] <- ability[i]
-		}
-		for (i in 2 : R) {
-			for (j in culm[i-1] + 1 : culm[i]) {
-				theta[j] <- ability[i]
-			}
-		}
-	# Rasch model
-		for (j in 1 : N) {
-			for (k in 1 : T) {
-				logit(p[j, k]) <- beta * theta[j] - alpha[k]
-				r[j, k] ~ dbern(p[j, k])
-			}
-		}
-	# Priors
-		for (k in 1 : T) {
-			alpha[k] ~ dnorm(0, 0.0001)
-			a[k] <- alpha[k] - mean(alpha[])
-		}
-		beta ~ dflat()T(0, )
-	}
+    model
+    {
+    # Calculate individual (binary) responses to each test from multinomial data
+        for (j in 1 : culm[1]) {
+            for (k in 1 : T) {
+                r[j, k] <- response[1, k]
+            }
+        }
+        for (i in 2 : R) {
+            for (j in culm[i - 1] + 1 : culm[i]) {
+                for (k in 1 : T) {
+                    r[j, k] <- response[i, k]
+                }
+            }
+        }
+    # Treat students with the same reponse profile as having the same latent ability
+        for (i in 1 : R) {
+            ability[i] ~ dnorm(0, 1)
+        }
+        for (j in 1 : culm[1]) {
+            theta[j] <- ability[i]
+        }
+        for (i in 2 : R) {
+            for (j in culm[i-1] + 1 : culm[i]) {
+                theta[j] <- ability[i]
+            }
+        }
+    # Rasch model
+        for (j in 1 : N) {
+            for (k in 1 : T) {
+                logit(p[j, k]) <- beta * theta[j] - alpha[k]
+                r[j, k] ~ dbern(p[j, k])
+            }
+        }
+    # Priors
+        for (k in 1 : T) {
+            alpha[k] ~ dnorm(0, 0.0001)
+            a[k] <- alpha[k] - mean(alpha[])
+        }
+        beta ~ dflat()T(0, )
+    }
 
 As a further trick, rather than forming the full student and response matrices,
 we simply multiply the response log-likelihood by the response frequency, which
@@ -107,11 +107,11 @@ problem.visible_vars = post_vars + ["beta"]
 
 
 openbugs_result = """
-		mean	sd	MC_error	val2.5pc	median	val97.5pc	start	sample
-	a[1]	-1.26	0.1053	0.001266	-1.474	-1.26	-1.056	1001	10000
-	a[2]	0.4776	0.0698	8.158E-4	0.3412	0.4776	0.6168	1001	10000
-	a[3]	1.239	0.0687	9.116E-4	1.106	1.239	1.374	1001	10000
-	a[4]	0.1696	0.07325	8.07E-4	0.02688	0.1692	0.313	1001	10000
-	a[5]	-0.6256	0.08617	0.001083	-0.7961	-0.6239	-0.4563	1001	10000
-	beta	0.7582	0.07181	0.001678	0.6125	0.7601	0.895	1001	10000
+         mean     sd       MC_error   2.5pc    median    97.5pc   start  sample
+  a[1]  -1.26     0.1053   0.001266  -1.474    -1.26     -1.056    1001   10000
+  a[2]   0.4776   0.0698   8.158E-4   0.3412    0.4776    0.6168   1001   10000
+  a[3]   1.239    0.0687   9.116E-4   1.106     1.239     1.374    1001   10000
+  a[4]   0.1696   0.07325  8.07E-4    0.02688   0.1692    0.313    1001   10000
+  a[5]  -0.6256   0.08617  0.001083  -0.7961   -0.6239   -0.4563   1001   10000
+  beta   0.7582   0.07181  0.001678   0.6125    0.7601    0.895    1001   10000
 """
