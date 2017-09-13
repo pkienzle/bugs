8f2ca5cc7f27bd6c507170dbbef1d715db412232
Paul Kienzle <pkienzle@nist.gov>
2017-09-13 14:49:57 -0400

    fix model so it now matches OpenBUGS

--- examples/_py/asia.py
+++ examples/_py/asia.py
@@ -14,8 +14,7 @@ Asia: expert system
         dyspnoea ~ dcat(p.dyspnoea[either,bronchitis,1:2])
     }
 """
-
-raise NotImplementedError("Model fails to reproduce the OpenBUGS result")
+from __future__ import print_function, division
 
 from bumps.names import *
 from bugs.parse import load, define_pars
@@ -30,11 +29,12 @@ pars = "smoking,tuberculosis,lung.cancer,bronchitis,xray".split(',')
 _, init = load('../Asiainits.txt')
 p0, labels = define_pars(init, pars)
 
-def asia(pars):
+def nllf(pars):
     smoking, tuberculosis, lung_cancer, bronchitis, xray \
-        = np.asarray(np.round(pars), 'i')
+        = np.asarray(np.floor(pars), 'i')
     cost = 0
     cost += dcat_llf(smoking, data['p.smoking'])
+    cost += dcat_llf(tuberculosis, data['p.tuberculosis'][data['asia']-1])
     cost += dcat_llf(lung_cancer, data['p.lung.cancer'][smoking-1])
     cost += dcat_llf(bronchitis, data['p.bronchitis'][smoking-1])
     either = max(tuberculosis, lung_cancer)
@@ -49,26 +49,45 @@ def post(pars):
 post_vars = ["either"]
 
 
-problem = DirectProblem(asia, p0, labels=labels, dof=1)
+problem = DirectProblem(nllf, p0, labels=labels, dof=1)
 problem.setp(p0)
 for i, p in enumerate(problem.p):
-    problem._bounds[:, i] = [0.500000001, 2.499999999]
+    problem._bounds[:, i] = [1.00000000, 2.999999999]
 problem.derive_vars = post, post_vars
 problem.visible_vars = [
     "bronchitis", "either", "lung.cancer", "smoking", "tuberculosis", "xray",
     ]
-
-import warnings
-warnings.warn("The Asia model contains integer parameters, which have not yet been shown to work in Bumps")
-#raise NotImplementedError("No support for integer variables")
-integer_vars = [
+problem.integer_vars = [
     "bronchitis", "either", "lung.cancer", "smoking", "tuberculosis", "xray",
-]
-# uncomment the following to treat integer variables as integers.  Need to
-# verify that the mean value is updated appropriately.  Make sure it is using
-# rounding since our bounds are [0.5, 2.5].
-problem.integer_vars = integer_vars
+    ]
 
+def brute_force():
+    llf = []
+    for smoking in (1, 2):
+        for tuberculosis in (1, 2):
+            for lung_cancer in (1, 2):
+                for bronchitis in (1, 2):
+                    for xray in (1, 2):
+                        p = np.asarray([smoking, tuberculosis, lung_cancer, bronchitis, xray])
+                        llf.append(-nllf(p))
+    p = np.exp(np.asarray(llf))
+    p = (p/np.sum(p)).reshape((2, 2, 2, 2, 2))
+    p_bronchitis = np.sum(p[:, :, :, 1, :])
+    p_lung_cancer = np.sum(p[:, :, 1, :, :])
+    p_smoking = np.sum(p[1, :, :, :, :])
+    p_tuberculosis = np.sum(p[:, 1, :, :, :])
+    p_xray = np.sum(p[:, :, :, :, 1])
+    p_either = p_tuberculosis + p_lung_cancer - np.sum(p[:, 1, 1, :, :])
+    rates = np.array([p_bronchitis, p_either, p_lung_cancer, p_smoking, p_tuberculosis, p_xray])
+    means = 1 + rates  # rates*2 + (1-rates)*1 = 1 + rates
+    print("""
+     bronchitis: %g
+         either: %g
+    lung_cancer: %g
+        smoking: %g
+   tuberculosis: %g
+           xray: %g
+    """%tuple(means.tolist()))
 
 openbugs_result = """
                mean    sd     median  2.5pc 97.5pc
@@ -79,3 +98,6 @@ smoking        1.626   0.4839    2     1     2
 tuberculosis   1.086   0.2798    1     1     2
 xray           1.218   0.4129    1     1     2
 """
+
+if __name__ == "__main__":
+    brute_force()
